a74315ae809e76de85a8437fe4d51c3bfef93390
cmagnus <cmagnus@gmail.com>
2017-04-27 13:56:16 -0400

    Adds ability to read input from file

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.io.Reader;
+import java.io.FileReader;
+
 public class Main{
   public static void main(String [ ] args) {
-    
+    String filename = "data.txt";
+    try
+    {
+      Scanner scanner = new Scanner(new FileReader(filename));
+
+      while (scanner.hasNext())
+      {
+        System.out.println(scanner.nextInt());
+      }
+      scanner.close();
+    }
+    catch (Exception e)
+    {
+      System.err.format("Exception occurred trying to read '%s'.", filename);
+      e.printStackTrace();
+    }
   }
 }
